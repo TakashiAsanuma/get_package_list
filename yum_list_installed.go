69f20b73ddfd46e75eb5f1175046509a1d1bcf5e
Takashi Asanuma <rmacchoj7@gmail.com>
2016-06-19 16:21:55 +0700

    execute go fmt

--- yum_list_installed.go
+++ yum_list_installed.go
@@ -1,70 +1,71 @@
 package main
+
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "os/exec"
-    "encoding/json"
-    "bytes"
-    "runtime"
-    "strings"
-    "regexp"
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"runtime"
+	"strings"
 )
 
 type Stats struct {
-        HostName       string `json:"host_name"`
-        HostOs         string `json:"host_os"`
-        PackageList    string `json:"package_list"`
+	HostName    string `json:"host_name"`
+	HostOs      string `json:"host_os"`
+	PackageList string `json:"package_list"`
 }
 
 func main() {
-    cmd := exec.Command("yum", "list", "installed")
-    stdout, err := cmd.StdoutPipe()
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	cmd := exec.Command("yum", "list", "installed")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    hostname, err := os.Hostname()
-    if err != nil {
-        fmt.Println("Get Hostname error:", err)
-        return
-    }
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println("Get Hostname error:", err)
+		return
+	}
 
-    cmd.Start()
+	cmd.Start()
 
-    lists := make([]string, 0, 10)
-    scanner := bufio.NewScanner(stdout)
-    i := 0
-    for scanner.Scan() {
-        //Exclude 2 line from stdout
-        if i > 1 {
-            lists = append(lists, scanner.Text())
-            if err := scanner.Err(); err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-            }
-        }
-        i = i+1
-    }
+	lists := make([]string, 0, 10)
+	scanner := bufio.NewScanner(stdout)
+	i := 0
+	for scanner.Scan() {
+		//Exclude 2 line from stdout
+		if i > 1 {
+			lists = append(lists, scanner.Text())
+			if err := scanner.Err(); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+		i = i + 1
+	}
 
-    list:= strings.Join(lists, " ")
-    rep := regexp.MustCompile(`[ ]+`)
-    list = rep.ReplaceAllString(list, " ")
+	list := strings.Join(lists, " ")
+	rep := regexp.MustCompile(`[ ]+`)
+	list = rep.ReplaceAllString(list, " ")
 
-    stats := &Stats{
-            HostName:       hostname,
-            HostOs:         runtime.GOOS,
-            PackageList:    list,
-    }
+	stats := &Stats{
+		HostName:    hostname,
+		HostOs:      runtime.GOOS,
+		PackageList: list,
+	}
 
-    jsonBytes, err := json.Marshal(stats)
-    if err != nil {
-        fmt.Println("JSON Marshal error:", err)
-        return
-    }
+	jsonBytes, err := json.Marshal(stats)
+	if err != nil {
+		fmt.Println("JSON Marshal error:", err)
+		return
+	}
 
-    out := new(bytes.Buffer)
-    json.Indent(out, jsonBytes, "", "    ")
-    fmt.Println(out.String())
+	out := new(bytes.Buffer)
+	json.Indent(out, jsonBytes, "", "    ")
+	fmt.Println(out.String())
 }
